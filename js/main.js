619a12b1a6f49e910a958033524da01982ba6e01
Jax Duan <j_duan168326@fanshaweonline.ca>
2023-01-13 11:17:01 -0500

    add js code

--- js/main.js
+++ js/main.js
@@ -1,4 +1,23 @@
 console.log('JavaScript is Running');
-let family = {dad, mom, me}
-dad = 'haha';
-mon =
+/* longer JS block comment 
+-conset is a type of variable - abit of memory that can hould a value 
+Js uses (can sue ) ANY vaild css selector  to make a connection to ANY
+element on the page - it works the same way as CSS selector do , because 
+It's USING css selectors to find macthing elements
+
+the document is the DOM - the web page and all of this elements querySelector is the method (funtion) that makes the connection between JS and the DOM (the elements)
+*/
+// 1. this is for make a connecction with bitmap
+const jpgGraphic = document.querySelector("#bitmap"); 
+const svgGraohic = document.querySelector("#vector");
+
+
+function logID () {
+    console.log('clicked on this element',this.id);
+
+}
+// 2. decide how you want to the user interact with the object you created in step 1
+jpgGraphic.addEventListener('click', logID); 
+svgGraohic.addEventListener('click',logID);
+
+
