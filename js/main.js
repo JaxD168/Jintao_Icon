1097ea64c8bfde31ca223161ea47ad7a2965ae2e
Jax Duan <j_duan168326@fanshaweonline.ca>
2023-01-14 15:36:37 -0500

    new
    new

--- js/main.js
+++ js/main.js
@@ -8,16 +8,16 @@ It's USING css selectors to find macthing elements
 the document is the DOM - the web page and all of this elements querySelector is the method (funtion) that makes the connection between JS and the DOM (the elements)
 */
 // 1. this is for make a connecction with bitmap
-const jpgGraphic = document.querySelector("#bitmap"); 
-const svgGraohic = document.querySelector("#vector");
+const jpgGraphic = document.querySelector('#bitmap'); 
+const svgGraohic = document.querySelector('#vector');
 
 
-function logID () {
+function logID () { //logid is a name for yourself
     console.log('clicked on this element',this.id);
 
 }
 // 2. decide how you want to the user interact with the object you created in step 1
-jpgGraphic.addEventListener('click', logID); 
+jpgGraphic.addEventListener('click',logID); 
 svgGraohic.addEventListener('click',logID);
 
 
